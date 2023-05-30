d3671e4f9c71dd6ba1ed395a2e0b2ceecbdc2fae
Hychiro (Hiero H B Costa) <hiero.costa@estudante.ufjf.br>
2023-05-30 20:21:48 -0300

    add elementos no vetor

--- main.js
+++ main.js
@@ -3,14 +3,25 @@ import store from "./store.js";
 const formulario = document.forms.entrada;
 
 formulario.addEventListener('submit', envia);
+
 atualiza();
+
 function envia(evento) {
     evento.preventDefault();
     console.log('forms enviado');
-    store.estado++;
+    const n= formulario.valor.value;
+    store.estado.push(n);
+    formulario.valor.value = "";
+    formulario.valor.focus();
     atualiza();
 }
 function atualiza() {
     const ol = document.querySelector('ol');
-    ol.innerHTML = `<li>${store.estado}</li>`;
+    ol.innerHTML = "";
+    for (let i = 0; i < store.estado.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = store.estado[i];
+        ol.appendChild(li);
+    }
+
 }
