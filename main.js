ac53022295ef8c29d448968536c3196e9d53bf6f
Hychiro (Hiero H B Costa) <hiero.costa@estudante.ufjf.br>
2023-05-30 19:50:03 -0300

    add store

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+import {estado} from "./store.js";
+
 const formulario = document.forms.entrada;
+
 formulario.addEventListener('submit', envia);
-let estado = 0
+atualiza();
 function envia(evento) {
     evento.preventDefault();
     console.log('forms enviado');
     estado++;
     atualiza();
 }
-function atualiza(){
+function atualiza() {
     const ol = document.querySelector('ol');
     ol.innerHTML = `<li>${estado}</li>`;
 }
